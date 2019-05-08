f06ff9402adfec76bd69ff048da3a9386ae3c639
Rahul Krishna <rkbalu524@gmail.com>
2019-05-08 15:04:35 +0530

    ✨ Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Summary from './components/Checkout/Summary';
+import UpdateCard from './components/Checkout/UpdateCard';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <Route exact path="/summary" component={Summary} />
+      <Route exact path="/update-card" component={UpdateCard} />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -13,7 +14,9 @@ const store = createStore(
 );
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
