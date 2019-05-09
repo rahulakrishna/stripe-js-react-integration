38d464a1ea914c017850827448e790186013bb8a
Rahul Krishna <rkbalu524@gmail.com>
2019-05-09 14:06:27 +0530

    ✨ Getting source instead of token

--- src/components/Checkout/UpdateCard.js
+++ src/components/Checkout/UpdateCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { CardElement, injectStripe } from 'react-stripe-elements';
+import { injectStripe } from 'react-stripe-elements';
 import { CardNumber, CardExpiry, Name, CardCVC, Inputline } from './cardStyles';
 
 class UpdateCard extends React.Component {
   submit = async () => {
     // this.props.stripe will have some stuff. Use that to whatever the fuck you want
-    let { token } = this.props.stripe.createToken({ name: 'Rahul Krishna' });
-    console.log({ token });
+    let { source } = await this.props.stripe.createSource({
+      type: 'card',
+    });
+    console.log({ source });
     // Now the frontend needs to send the backend token.id
   };
   render() {
